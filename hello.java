a6bb528a943aa4ae72de9cc8120fc4524ad7fa16
Riley <riley.cassidy@maine.edu>
2021-02-03 09:44:09 -0500

    Goodbye
    Added short goodbye message

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Here ye : important announcement");
         System.out.println("Hello World"); 
+        System.out.println("Goodbye cruel world"); 
     }
 }
