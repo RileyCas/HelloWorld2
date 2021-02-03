194953916792a7719312da3af0f41add26b59b64
Riley <riley.cassidy@maine.edu>
2021-02-03 09:54:54 -0500

    Bob
    Say hello to Bob
    This change was done on the same line as saying hello to Alice

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Here ye : important announcement");
         System.out.println("Hello World"); 
+        System.out.println("Hello Bob");
         System.out.println("Goodbye cruel world"); 
     }
 }
