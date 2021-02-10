a68323d70af57e6275b1a9c086a904c59ed3f85c
Riley <riley.cassidy@maine.edu>
2021-02-10 10:02:50 -0500

    Display time - issue 1
    Closes #1

--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
+import java.time.LocalTime;
+
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Here ye : important announcement");
+        System.out.println("The current time is " + LocalTime.now());
         System.out.println("Hello World"); 
         System.out.println("Hello Alice");
         System.out.println("Hello Bob");
